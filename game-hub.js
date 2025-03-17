c8bc3979c5d56f6d3ce8f4c6349adacb3a7328e9
Shreycodesbetter <sagaramte@gmail.com>
2025-03-17 11:17:03 -0700

    Update game-hub.js

--- game-hub.js
+++ game-hub.js
@@ -3,8 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const gameDetailsSection = document.getElementById("game-details");
     const gameInfoDiv = document.getElementById("game-info");
     const playGameBtn = document.getElementById("play-game-btn");
-    const gameFrameSection = document.getElementById("game-frame");
-    const gameIframe = document.getElementById("game-iframe");
 
     games.forEach(game => {
         game.addEventListener("click", function() {
@@ -13,44 +11,28 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-    playGameBtn.addEventListener("click", function() {
-        const gameName = playGameBtn.getAttribute("data-game");
-        playGame(gameName);
-    });
-
     function loadGameDetails(gameName) {
         let gameDetails = "";
+        let gameUrl = "";
         switch (gameName) {
             case "fishing-game":
                 gameDetails = `
                     <h3>Fishing Game</h3>
                     <p>Try to catch as many fish as you can and upgrade your rod to catch even more valuable fish!</p>
                 `;
+                gameUrl = "https://shreycodesbetter.github.io/Fisch/";
                 break;
             case "snow-rider-3d":
                 gameDetails = `
                     <h3>Snow Rider 3D</h3>
                     <p>Enjoy a thrilling snow ride and avoid obstacles to reach the end!</p>
                 `;
+                gameUrl = "https://sites.google.com/view/unblocked-games-x6/snow-rider-3d";
                 break;
             // Add more cases for other games
         }
         gameInfoDiv.innerHTML = gameDetails;
-        playGameBtn.setAttribute("data-game", gameName);
+        playGameBtn.href = gameUrl;
         gameDetailsSection.classList.remove("hidden");
-    }
-
-    function playGame(gameName) {
-        switch (gameName) {
-            case "fishing-game":
-                gameIframe.src = "https://shreycodesbetter.github.io/Fisch/";
-                gameFrameSection.classList.remove("hidden");
-                break;
-            case "snow-rider-3d":
-                gameIframe.src = "https://sites.google.com/site/thegamecompilation/snow-rider-3d#h.29qvw5o0hrzl";
-                gameFrameSection.classList.remove("hidden");
-                break;
-            // Add cases to load other games
-        }
     }
 });
